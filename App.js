df75dbfe8f00d9838f35c434074c82b6c33e1332
007arunwilson <007arunwilson@gmail.com>
2018-06-13 21:06:16 +0530

    Cognito Progressing ..

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, AsyncStorage, Alert } from "react-native";
+import { Platform, StyleSheet, Text, View, AsyncStorage, Alert, Button } from "react-native";
 import axios from "axios";
 import cloneDeep from "lodash/cloneDeep";
 import AWS from "aws-sdk";
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n Cmd+D or shake for dev menu",
-  android: "Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu"
+  ios: "The best IOT netwoking application in IOS",
+  android: "The best IOT netwoking application in IOS"
 });
 
 const styles = StyleSheet.create({
@@ -38,67 +38,73 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    //this.fetchAwsCredentials();
-    //this.storageGetItem("aws_credetnails", "1").then(result => console.log("Result : ", result));
+    const cognitoInitiater = new Promise((resolve, reject) => {
+      this.getAWSCredentialsFromStorage()
+        .then(result => {
+          this.initiateAWS(result);
+          resolve();
+        })
+        .catch(() => {
+          this.fetchAwsCredentials()
+            .then(response => {
+              let appState = null;
 
-    // AsyncStorage.getItem("@AppAWSCredential:").then((result, error) => {
-    //   console.log("Result : ", result);
-    //   console.log("Error : ", error);
-    // });
+              this.setState(state => {
+                const clonedState = cloneDeep(state);
+                clonedState.cognitoCredentials = response.data;
+                appState = clonedState;
+                return clonedState;
+              });
 
-    this.getAWSCredentialsFromStorage()
-      .then(result => {
-        this.initiateAWS(result);
-        this.doAwsProcess();
-      })
-      .catch(() => {
-        this.fetchAwsCredentials()
-          .then(response => {
-            console.log("Cognito Result : ", response);
-
-            let appState = null;
-
-            this.setState(state => {
-              const clonedState = cloneDeep(state);
-              clonedState.coginated = true;
-              clonedState.cognitoCredentials = response.data;
-              appState = clonedState;
-              return clonedState;
+              this.saveAWSCredentialsToStorage(appState.cognitoCredentials);
+              this.initiateAWS(appState.cognitoCredentials);
+              resolve();
+            })
+            .catch(error => {
+              reject(error);
             });
+        });
+    });
+    cognitoInitiater
+      .then(() => {
+        const AWSCredentials = AWS.Credentials();
 
-            this.saveAWSCredentialsToStorage(appState.cognitoCredentials);
-            this.initiateAWS(appState.cognitoCredentials);
-            this.doAwsProcess();
-          })
-          .catch(error => {
-            console.log("Cognito Aciox Error : ", error);
-            this.triggerErrorAlert(error);
-          });
-      });
+        console.log(AWSCredentials);
 
-    // AsyncStorage.setItem("@MySuperStore:numbers", "8086699702").then((result, error) => {
-    //   console.log("Result : ", result);
-    //   console.log("Error : ", error);
-    // });
-
-    // this.storageGetItem("aws_credetnails").then(result => {
-    //   console.log("result : ", result);
-    // });
+        // this.setState(state => {
+        //   const clonedState = cloneDeep(state);
+        //   clonedState.coginated = true;
+        //   return clonedState;
+        // });
+      })
+      .catch(error => this.triggerErrorAlert(error));
   }
 
   getAWSCredentialsFromStorage = () => {
     const awsCredentialsObject = {};
 
     return new Promise((resolve, reject) => {
-      AsyncStorage.getItem("@MySuperStore:identityPoolId")
+      AsyncStorage.getItem("@AppAWSCredentinal:identityPoolId")
         .then(identityPoolId => {
+          if (!identityPoolId) {
+            return reject();
+          }
+
           awsCredentialsObject.identityPoolId = identityPoolId;
-          AsyncStorage.getItem("@MySuperStore:identityId")
+          AsyncStorage.getItem("@AppAWSCredentinal:identityId")
             .then(identityId => {
-              awsCredentialsObject.identityPoolId = identityId;
-              AsyncStorage.getItem("@MySuperStore:tokenId")
+              if (!identityId) {
+                return reject();
+              }
+
+              awsCredentialsObject.identityId = identityId;
+              AsyncStorage.getItem("@AppAWSCredentinal:tokenId")
                 .then(tokenId => {
-                  awsCredentialsObject.identityPoolId = tokenId;
+                  awsCredentialsObject.tokenId = tokenId;
+                  if (!tokenId) {
+                    return reject();
+                  }
+
                   resolve(awsCredentialsObject);
                 })
                 .catch(error => reject(error));
@@ -112,7 +118,7 @@ export default class App extends Component {
   saveAWSCredentialsToStorage = cognitoCredentials => {
     const cognitoCredentialsKey = Object.keys(cognitoCredentials);
     cognitoCredentialsKey.forEach(ele => {
-      AsyncStorage.setItem(`@MySuperStore:${ele}`, cognitoCredentials[cognitoCredentials]);
+      AsyncStorage.setItem(`@AppAWSCredentinal:${ele}`, cognitoCredentials[ele]);
     });
   };
 
@@ -134,42 +140,48 @@ export default class App extends Component {
       }),
       region: "us-east-1"
     });
+
+    // this.doAwsProcess();
   };
 
   doAwsProcess = () => {
-    console.log("do AWS Process trigered ..");
+    const AWS_S3 = new AWS.S3();
 
-    // const AWS_S3 = new AWS.S3();
-
-    // AWS_S3.getBucketCors(
-    //   {
-    //     Bucket: "cyberinfoscripter"
-    //   },
-    //   (error, data) => {
-    //     if (error) {
-    //       console.log("Error occured : ", error);
-    //     } else {
-    //       console.log("Buckets data : ", data);
-    //     }
-    //   }
-    // );
+    AWS_S3.getBucketCors(
+      {
+        Bucket: "cyberinfoscripter"
+      },
+      (error, data) => {
+        if (error) {
+          const originalError = error.originalError;
+          console.log(originalError);
+          this.triggerErrorAlert(originalError);
+        } else {
+          console.log("Buckets data : ", data);
+        }
+      }
+    );
   };
 
   triggerErrorAlert = error => {
     Alert.alert(
       "Oops! Some thing went wrong",
       `Error detail : ${error.message ? error.message : "Not Available"}`,
-      [{ text: "Cancel", onPress: () => console.log("Cancel Pressed"), style: "cancel" }],
-      { cancelable: true }
+      [{ text: "Cancel", style: "cancel" }],
+      { cancelable: false }
     );
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>Hub.io</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+          disabled={!this.state.coginated}
+          onPress={this.doAwsProcess}
+          title={this.state.coginated ? "Do AWS task" : "Cognitating..."}
+        />
       </View>
     );
   }

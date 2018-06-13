d43cffd05922a3fa5ff106674f6f0b587d1c5cf0
007arunwilson <007arunwilson@gmail.com>
2018-06-13 13:24:45 +0530

    Balck Comit

--- App.js
+++ App.js
@@ -1,58 +1,111 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, AsyncStorage } from "react-native";
+import axios from "axios";
+import AWS from "aws-sdk";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchAwsCrdentials = null;
+    let Console: {
+      new(): Console,
+      prototype: Console
+    };
+    this.console = Console;
+  }
+
+  componentDidMount() {
+    this.fetchAwsCrdentials();
+  }
+
+  fetchAwsCrdentials = () => {
+    this.console.log("fetching of AWS credentials initated");
+    axios
+      .post(
+        "https://47hith9kh1.execute-api.us-east-1.amazonaws.com/prod/get-cognito",
+        {
+          developerProviderName: "cognito",
+          userId: "48"
+        }
+      )
+      .then(response => {
+        this.console.log("Cognito Result : ", response);
+
+        const stateVar = null;
+
+        this.setState(state => {
+          const clonedState = cloneDeep(state);
+          clonedState.coginated = true;
+          clonedState.cognitoCredentials = response.data;
+          return clonedState;
+        });
+
+        this.saveAwsCredentialsToASync(stateVar.cognitoCredentials);
+      })
+      .catch(error => {
+        this.console.log("Cognito Error : ", error);
+      });
+  };
+
+  storageSetItem = async (key_, value_) =>
+    new Promise(async (resolve, reject) => {
+      try {
+        await AsyncStorage.setItem(`@MySuperStore:${key_}`, value_, () => {
+          resolve(value_);
+        });
+      } catch (error) {
+        this.console.log("storageSetItem save error : ", error);
+        reject(error);
+      }
+    });
+
+  storageGetItem = async key_ =>
+    new Promise(async (resolve, reject) => {
+      try {
+        await AsyncStorage.getItem(`@MySuperStore:${key_}`, (error, resolt) => {
+          if (error) {
+            throw error;
+          } else {
+          }
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
